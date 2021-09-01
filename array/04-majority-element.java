bc5e2ab8596775ce71f7d18cdb631213d59b3f72
poseidon-code <pritamhalder05062000@gmail.com>
2021-09-01 12:51:42 +0530

    concise: array-04

--- array/04-majority-element.java
+++ array/04-majority-element.java
@@ -10,12 +10,12 @@ class Solution {
         int major=nums[0];
         int count=1;
 
-        for (int i=1; i<nums.length; i++) {
+        for (int n : nums) {
             if (count == 0) {
                 count++;
-                major = nums[i];
+                major = n;
             }
-            else if (major == nums[i])
+            else if (major == n)
                 count++;
             else
                 count--;
