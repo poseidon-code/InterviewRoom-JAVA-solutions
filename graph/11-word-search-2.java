cc59e97da07b37201952708b238e34bcf14c4931
poseidon-code <pritamhalder05062000@gmail.com>
2022-01-04 12:20:50 +0530

    add: g-11

---
+++ graph/11-word-search-2.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// PROBLEM: Word Search 2
+// https://leetcode.com/problems/word-search-ii/
+// https://practice.geeksforgeeks.org/problems/word-boggle4143/1/?category[]=Graph&category[]=Graph&page=2&query=category[]Graphpage2category[]Graph
+
+class Solution {
+    // SOLUTION
+    class TrieNode {
+        TrieNode[] next = new TrieNode[26];
+        String word;
+    }
+    
+    public TrieNode buildTrie(String[] words) {
+        TrieNode root = new TrieNode();
+        for (String w : words) {
+            TrieNode p = root;
+            for (char c : w.toCharArray()) {
+                int i = c - 'a';
+                if (p.next[i] == null) p.next[i] = new TrieNode();
+                p = p.next[i];
+           }
+           p.word = w;
+        }
+        return root;
+    }
+
+    public void dfs (char[][] board, int i, int j, TrieNode p, List<String> result) {
+        char c = board[i][j];
+        if (c == '#' || p.next[c - 'a'] == null) return;
+        
+        p = p.next[c - 'a'];
+        if (p.word != null) {
+            result.add(p.word);
+            p.word = null;
+        }
+
+        board[i][j] = '#';
+        if (i > 0) dfs(board, i - 1, j ,p, result); 
+        if (j > 0) dfs(board, i, j - 1, p, result);
+        if (i < board.length - 1) dfs(board, i + 1, j, p, result); 
+        if (j < board[0].length - 1) dfs(board, i, j + 1, p, result); 
+        board[i][j] = c;
+    }
+
+    public List<String> find_words (char[][] board, String[] words) {
+        List<String> result = new ArrayList<>();
+        TrieNode root = buildTrie(words);
+
+        for (int i=0; i<board.length; i++) {
+            for (int j = 0; j<board[0].length; j++) {
+                dfs (board, i, j, root, result);
+            }
+        }
+
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        // INPUT :
+        char[][] board = {
+            {'o','a','a','n'},
+            {'e','t','a','e'},
+            {'i','h','k','r'},
+            {'i','f','l','v'}
+        };
+        String[] words = {"oath","pea","eat","rain"};
+
+        // OUTPUT :
+        var result = s.find_words(board, words);
+        System.out.print("["); for (var x : result) System.out.print(x+", "); System.out.println("\b\b]");
+    }
+}
